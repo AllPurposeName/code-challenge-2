af53f8e3b70f1742b719c175bfe0123e1376318c
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-06-16 10:06:54 -0600

    Board now validates title

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class BoardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_it_requires_a_title
+    board = Board.new
+    board_with_title = Board.new(title: "Hello World")
+
+    refute board.save
+    assert board_with_title.save
+  end
 end
